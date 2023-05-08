103963cb5a2a1f17cef4e6a43afe96ca143a20d7
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-05-08 14:45:34 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { App } from 'components/App';
+import theme from 'muiTheme/theme';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
+import { persistor, store } from './redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Router basename="/MedDoc">
+                        <App />
+                    </Router>
+                </ThemeProvider>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
+);
